9480506ff2196cf451a254ac808b2a7c296870b6
yuzu00117 <66988732+yuzu00117@users.noreply.github.com>
2021-02-05 15:12:27 +0900

    BoardGameProject

---
+++ Assets/Script/Lazer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class Lazer : MonoBehaviour {
+
+    GameObject ClickedPiece;
+    GameObject HaveObject = null;
+
+    void Start() {
+        var a = this.gameObject.GetComponents<Component>();
+        foreach (var item in a) {
+        }
+    }
+
+    void Update() {
+
+        if ( Input.GetKeyDown( KeyCode.RightArrow ) && HaveObject != null ) {
+            HaveObject.transform.rotation = Quaternion.Euler(-90, -90, 0);
+            HaveObject = null;
+            GetComponent<Renderer>().material.color = Color.white;
+        }
+        
+        if ( Input.GetKeyDown ( KeyCode.LeftArrow ) && HaveObject != null ) {
+            HaveObject.transform.rotation = Quaternion.Euler(-90, -90, -90);
+            HaveObject = null;
+            GetComponent<Renderer>().material.color = Color.white;
+        }
+
+        if ( Input.GetMouseButtonDown( 0 ) ) {
+            ClickedPiece = null;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+
+            if ( Physics.Raycast( ray, out hit ) ) {
+                ClickedPiece = hit.collider.gameObject;
+
+                if ( ClickedPiece.name == "WhiteLazer" && HaveObject == null ) {
+                   GetComponent<Renderer>().material.color = Color.yellow;
+                    HaveObject = ClickedPiece;
+                    Debug.Log( "white lazer is clicked!" );
+                }
+
+                if ( ClickedPiece.name == "RedLazer" && HaveObject == null ) {
+                    GetComponent<Renderer>().material.color = Color.yellow;
+                    HaveObject = ClickedPiece;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Script/MoveSelector.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSelector : MonoBehaviour {
+
+    public void Test_Fun() {
+        Debug.Log( "テスト用関数の呼び出しに成功" );
+    }
+}
