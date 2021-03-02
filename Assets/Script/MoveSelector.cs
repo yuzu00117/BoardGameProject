6a68a3d2d54d37ced71febaecf2cd7fa8a8fdce3
yuzu00117 <66988732+yuzu00117@users.noreply.github.com>
2021-03-02 18:57:39 +0900

    BoardGamePtoject

--- Assets/Script/MoveSelector.cs
+++ Assets/Script/MoveSelector.cs
@@ -4,14 +4,32 @@ using UnityEngine;
 
 public class MoveSelector : MonoBehaviour {
 
-    void Start() {
-        Debug.Log( "MoveSelector has enabled" );
-    }
-    
-    void OnTrrigerStay( Collider other ) {
-        // 接触しているオブジェクトがマスだったらそのマスをtrueにする
-        if ( other.gameObject.tag == "Space" ) {
-        Debug.Log( "マスを発見しました");
+    GameObject TileObj;
+    GameObject otherTile;
+    GameObject LastTile;
+
+// OnTriggerStay でマスを探してハイライトする
+    void OnTriggerStay( Collider other ) {
+        if ( other.gameObject.CompareTag( "Space" ) ) {
+            TileObj = GameObject.Find( other.name );
+            Debug.Log( TileObj );
+            EnableHighLight();
+            Invoke( "DisableOtherTile", 0.1f );
         }
+    }
+
+    void Disablehighlight() {
+
+    }
+
+    // 周囲8マス意外のマスへの移動を無効化する
+    void DisableOtherTile() {
+        otherTile = GameObject.FindWithTag( "Space" );
+        otherTile.SetActive( false );
+        Debug.Log( otherTile );
+    }
+
+    void EnableHighLight() {
+        TileObj.tag = "SelectedTile";
     }
 }
